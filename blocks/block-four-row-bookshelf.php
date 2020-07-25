75718175f2872c5437f809ad004b90b133650eef
Pablo Defendini <pablo@defendini.com>
2020-07-25 15:16:57 -0400

    block lab blocks for bookshelves

---
+++ blocks/block-four-row-bookshelf.php
@@ -0,0 +1,36 @@
+<div class="bookshelf">
+  <ul>
+    <li class="book">
+      <figure>
+        <img src="<?php block_field( 'book-1-cover' ); ?>"/>
+      </figure>
+      <h1><a href="<?php block_field( 'book-1-link' ); ?>"><?php block_field( 'book-1-title' ); ?></a></h1>
+      <p><?php block_field( 'book-1-byline' ); ?></p>
+      <p><?php block_field( 'book-1-description' ); ?></p>
+    </li>
+    <li class="book">
+      <figure>
+        <img src="<?php block_field( 'book-2-cover' ); ?>"/>
+      </figure>
+      <h1><a href="<?php block_field( 'book-2-link' ); ?>"><?php block_field( 'book-2-title' ); ?></a></h1>
+      <p><?php block_field( 'book-2-byline' ); ?></p>
+      <p><?php block_field( 'book-2-description' ); ?></p>
+    </li>
+    <li class="book">
+      <figure>
+        <img src="<?php block_field( 'book-3-cover' ); ?>"/>
+      </figure>
+      <h1><a href="<?php block_field( 'book-3-link' ); ?>"><?php block_field( 'book-3-title' ); ?></a></h1>
+      <p><?php block_field( 'book-3-byline' ); ?></p>
+      <p><?php block_field( 'book-3-description' ); ?></p>
+    </li>
+    <li class="book">
+      <figure>
+        <img src="<?php block_field( 'book-4-cover' ); ?>"/>
+      </figure>
+      <h1><a href="<?php block_field( 'book-4-link' ); ?>"><?php block_field( 'book-4-title' ); ?></a></h1>
+      <p><?php block_field( 'book-4-byline' ); ?></p>
+      <p><?php block_field( 'book-4-description' ); ?></p>
+    </li>
+  </ul>
+</div>
