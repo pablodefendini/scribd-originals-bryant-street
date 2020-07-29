334c82f84f1c1d9498722f8cde150ed73385d8df
Pablo Defendini <pablo@defendini.com>
2020-07-29 09:10:56 -0400

    ad layouts blocks and styles

---
+++ blocks/block-ad-horizontal.php
@@ -0,0 +1,7 @@
+<div class="ad ad-horizontal" style="
+      background-image:url(<?php block_field( 'background-image' ); ?>);
+      background-size: cover;
+      background-repeat:no-repeat;">
+  <h1><?php block_field( 'headline' ); ?></h1>
+  <p><?php block_field( 'tagline' ); ?></p>
+</div>
---
+++ blocks/block-ad-vertical.php
@@ -0,0 +1,8 @@
+<div class="ad ad-vertical" style="
+      background-image:url(<?php block_field( 'background-image' ); ?>);
+      background-size: cover;
+      background-position: center;
+      background-repeat:no-repeat;">
+  <h1><?php block_field( 'headline' ); ?></h1>
+  <p><?php block_field( 'tagline' ); ?></p>
+</div>
