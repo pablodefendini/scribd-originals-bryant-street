8a3b3651ab80cf5a50d823afa531d60fb463fa8b
Pablo Defendini <pablo@defendini.com>
2020-07-30 08:09:26 -0400

    add links to books

--- blocks/block-four-row-bookshelf.php
+++ blocks/block-four-row-bookshelf.php
@@ -7,6 +7,10 @@
       <h1><a href="<?php block_field( 'book-1-link' ); ?>"><?php block_field( 'book-1-title' ); ?></a></h1>
       <p><?php block_field( 'book-1-byline' ); ?></p>
       <p><?php block_field( 'book-1-description' ); ?></p>
+      <div class="bookshelf-links">
+        <a href="<?php block_field( 'book-1-read-link' ); ?>"><p>Read Now</p></a>
+        <a href="<?php block_field( 'book-1-audio-link' ); ?>"><p>Listen Now</p></a>
+      </div>
     </li>
     <li class="book">
       <figure>
@@ -15,6 +19,10 @@
       <h1><a href="<?php block_field( 'book-2-link' ); ?>"><?php block_field( 'book-2-title' ); ?></a></h1>
       <p><?php block_field( 'book-2-byline' ); ?></p>
       <p><?php block_field( 'book-2-description' ); ?></p>
+      <div class="bookshelf-links">
+        <a href="<?php block_field( 'book-2-read-link' ); ?>"><p>Read Now</p></a>
+        <a href="<?php block_field( 'book-2-audio-link' ); ?>"><p>Listen Now</p></a>
+      </div>
     </li>
     <li class="book">
       <figure>
@@ -23,6 +31,10 @@
       <h1><a href="<?php block_field( 'book-3-link' ); ?>"><?php block_field( 'book-3-title' ); ?></a></h1>
       <p><?php block_field( 'book-3-byline' ); ?></p>
       <p><?php block_field( 'book-3-description' ); ?></p>
+      <div class="bookshelf-links">
+        <a href="<?php block_field( 'book-3-read-link' ); ?>"><p>Read Now</p></a>
+        <a href="<?php block_field( 'book-3-audio-link' ); ?>"><p>Listen Now</p></a>
+      </div>
     </li>
     <li class="book">
       <figure>
@@ -31,6 +43,10 @@
       <h1><a href="<?php block_field( 'book-4-link' ); ?>"><?php block_field( 'book-4-title' ); ?></a></h1>
       <p><?php block_field( 'book-4-byline' ); ?></p>
       <p><?php block_field( 'book-4-description' ); ?></p>
+      <div class="bookshelf-links">
+        <a href="<?php block_field( 'book-4-read-link' ); ?>"><p>Read Now</p></a>
+        <a href="<?php block_field( 'book-4-audio-link' ); ?>"><p>Listen Now</p></a>
+      </div>
     </li>
   </ul>
 </div>
