a7df060217e0c7193172facd943fa26442a6aae1
Pablo Defendini <pablo@defendini.com>
2020-07-30 06:34:36 -0400

    fixes to ads

--- blocks/block-ad-horizontal.php
+++ blocks/block-ad-horizontal.php
@@ -1,7 +1,8 @@
 <div class="ad ad-horizontal" style="
       background-image:url(<?php block_field( 'background-image' ); ?>);
       background-size: cover;
-      background-repeat:no-repeat;">
+      background-repeat:no-repeat;
+      background-color: <?php block_field( 'background-color' ); ?>">
   <h1><?php block_field( 'headline' ); ?></h1>
   <p><?php block_field( 'tagline' ); ?></p>
 </div>
--- blocks/block-ad-vertical.php
+++ blocks/block-ad-vertical.php
@@ -2,7 +2,8 @@
       background-image:url(<?php block_field( 'background-image' ); ?>);
       background-size: cover;
       background-position: center;
-      background-repeat:no-repeat;">
+      background-repeat:no-repeat;
+      background-color: <?php block_field( 'background-color' ); ?>">
   <h1><?php block_field( 'headline' ); ?></h1>
   <p><?php block_field( 'tagline' ); ?></p>
 </div>
