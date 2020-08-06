0db8982388d2ba2812ecc925d9948b117567216e
Pablo Defendini <pablo@defendini.com>
2020-08-06 07:41:54 -0400

    clean up

--- header.php
+++ header.php
@@ -53,46 +53,33 @@
 				<div id="inner-header" class="wrap cf">
 
 					<?php // to use a image just replace the bloginfo('name') with your img src and remove the surrounding <p> ?>
-						<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo home_url(); ?>/wp-content/themes/scribd-originals-bryant-street/library/images/logo.svg" id=logo ></a>
+					<a href="<?php echo home_url(); ?>" rel="nofollow"><img src="<?php echo home_url(); ?>/wp-content/themes/scribd-originals-bryant-street/library/images/logo.svg" id=logo ></a>
 
+					<?php  if ( is_page('Bryant Street Pub Newsletter') ) { ?>
+					<div></div>
+					<?php  } else { ?>
+					<!-- Begin Mailchimp Signup Form -->
+					<div id="mc_embed_signup">
+						<form action="https://bryantstreetpub.us17.list-manage.com/subscribe/post?u=494eab8650d545c29ccbdd962&amp;id=a7c855f18e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
+						    <div id="mc_embed_signup_scroll">
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Newsletter signup" required>
+						    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
+						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0c60ee4cf297215c61d55e861_afc8f5cf09" tabindex="-1" value=""></div>
+						    <div class="clear"><input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						    </div>
+						</form>
+					</div>
+					<!--End mc_embed_signup-->
+					<?php  } ?>
 
-					<?php // if you'd like to use the site description you can un-comment it below ?>
-					<?php // bloginfo('description'); ?>
+					<nav id="#hamnav" role="navigation">
 
-					<!--div class="sign-and-nav"-->
+						<label for="hamburger" id="hamburgericon">&#9776;</label>
+    				<input type="checkbox" id="hamburger"/>
 
-						<?php  if ( is_page('Bryant Street Pub Newsletter') ) { ?>
-							<div></div>
-						<?php  } else { ?>
+						<?php wp_nav_menu(array('menu' => __( 'Main Menu'), 'theme_location' => 'main_nav', 'container' => '', 'menu_id' => 'hamitems')); ?>
 
-							<!-- Begin Mailchimp Signup Form -->
-
-							<div id="mc_embed_signup">
-								<form action="https://bryantstreetpub.us17.list-manage.com/subscribe/post?u=494eab8650d545c29ccbdd962&amp;id=a7c855f18e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
-								    <div id="mc_embed_signup_scroll">
-									<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Newsletter signup" required>
-								    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
-								    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0c60ee4cf297215c61d55e861_afc8f5cf09" tabindex="-1" value=""></div>
-								    <div class="clear"><input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
-								    </div>
-								</form>
-							</div>
-
-							<!--End mc_embed_signup-->
-
-						<?php  } ?>
-						<nav id="#hamnav" role="navigation">
-
-							<label for="hamburger" id="hamburgericon">&#9776;</label>
-      				<input type="checkbox" id="hamburger"/>
-
-							<?php wp_nav_menu(array('menu' => __( 'Main Menu'), 'theme_location' => 'main_nav', 'container' => '', 'menu_id' => 'hamitems')); ?>
-
-
-						</nav>
-
-						</nav>
-					<!--/div-->
+					</nav>
 
 				</div>
 
