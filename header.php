1d5dafaff5274c62a1f411c662182bb75514b84a
Pablo Defendini <pablo@defendini.com>
2020-08-06 07:43:15 -0400

    newsletter copy change

--- header.php
+++ header.php
@@ -62,10 +62,10 @@
 					<div id="mc_embed_signup">
 						<form action="https://bryantstreetpub.us17.list-manage.com/subscribe/post?u=494eab8650d545c29ccbdd962&amp;id=a7c855f18e" method="post" id="mc-embedded-subscribe-form" name="mc-embedded-subscribe-form" class="validate" target="_blank" novalidate>
 						    <div id="mc_embed_signup_scroll">
-							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="Newsletter signup" required>
+							<input type="email" value="" name="EMAIL" class="email" id="mce-EMAIL" placeholder="your email here" required>
 						    <!-- real people should not fill this in and expect good things - do not remove this or risk form bot signups-->
 						    <div style="position: absolute; left: -5000px;" aria-hidden="true"><input type="text" name="b_0c60ee4cf297215c61d55e861_afc8f5cf09" tabindex="-1" value=""></div>
-						    <div class="clear"><input type="submit" value="Sign Up" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
+						    <div class="clear"><input type="submit" value="Sign up for BSP’s newsletter" name="subscribe" id="mc-embedded-subscribe" class="button"></div>
 						    </div>
 						</form>
 					</div>
